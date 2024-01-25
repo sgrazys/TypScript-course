be643dcbfd5974d970b166fc64416bbc02615a35
sgrazys <s.grazys@gmail.com>
2024-01-25 18:49:58 +0200

    intersections

--- advanced-ts/src/app.ts
+++ advanced-ts/src/app.ts
@@ -1,1 +1,19 @@
-console.log(123);
+type Admin = {
+	name: string;
+	privileges: string[];
+};
+
+type Employee = {
+	name: string;
+	startDate: Date;
+};
+
+type ElevatedEmployee = Admin & Employee;
+
+const e1: ElevatedEmployee = {
+	name: 'Tauras',
+	privileges: ['Admin', 'Editor'],
+	startDate: new Date('2018 7 02 16:45:00'),
+};
+
+console.log(e1.startDate);
